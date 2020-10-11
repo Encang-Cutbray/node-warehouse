2e1907073bb2ab3489f46972256a4fd16c745901
encang cutbray <aipachtarudin@gmail.com>
2020-10-11 20:36:15 +0700

    remove alert with setimeout

--- app-warehouse/src/routes/auth.route.ts
+++ app-warehouse/src/routes/auth.route.ts
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get('/login', guest, authController.getLogin);
 
 router.post('/login', guest, [
-	check('email').isEmail(),
-	check('password').notEmpty()
+	check('email', 'email is required').isEmail(),
+	check('password', 'password is required').notEmpty()
 ], authController.postLogin);
 
 router.get('/register', guest, authController.getRegister);
@@ -17,17 +17,19 @@ router.post('/register', guest, [
 	check('name', 'name is required').notEmpty(),
 	check('email', 'email is invalid').isEmail(),
 	check('password', 'password is required').notEmpty(),
-	check('confirm', 'confirm password not match').custom((value, {req}) => {		
-		if (req.body.password !== value){
+	check('confirm', 'confirm password not match').custom((value, { req }) => {
+		if (req.body.password !== value) {
 			return false
 		}
-		return true		
+		return true
 	})
 ], authController.postRegister);
 
 router.get('/forgot-password', guest, authController.getForgotPassword);
 
-router.post('/forgot-password', guest, authController.postForgotPassword);
+router.post('/forgot-password', guest, [
+	check('email', 'email is invalid').isEmail()
+], authController.postForgotPassword);
 
 router.get('/logout', auth, authController.getLogout);
 
